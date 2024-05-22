f4c8b1221c3664dac6a97cb1731fcc1a9e0db7ac
jacobchristeena <jacob.chr@northeastern.edu>
2024-05-22 14:12:11 -0400

    one three

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,7 +7,14 @@ export default function TOC() {
         <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
         <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
         <li><a id="wd-k"  href="#/Kanbas">Kanbas</a></li>
-        <li><a id="github" href="https://github.com/jacobchristeena18/kanbas-web/tree/a1" target="_blank">GitHub</a></li>
+        <li>
+        <a
+          id="wd-github"
+          href="https://github.com/jacobchristeena18/kanbas-web/tree/a1"
+        >
+          GitHub
+        </a>
+      </li>
       </ul>
     );
   }
