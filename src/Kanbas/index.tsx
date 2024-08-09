7c967e1a6267802d4666e48bf4bd3f5926bf995e
jacobchristeena <jacob.chr@northeastern.edu>
2024-08-09 12:47:23 -0400

    a6new

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import { Provider } from "react-redux";
 import { Routes, Route, Navigate } from "react-router";
 import axios from "axios";
+import ProtectedRoute from "./ProtectedRoute";
 import store from "./store";
+import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
@@ -67,8 +69,9 @@ export default function Kanbas() {
           <div className="flex-fill p-4">
             <Routes>
               <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="/Account/*" element={<Account />} />
               <Route path="Dashboard" element={
+                <ProtectedRoute>
                 <Dashboard
                   courses={courses}
                   course={course}
@@ -77,8 +80,9 @@ export default function Kanbas() {
                   deleteCourse={deleteCourse}
                   updateCourse={updateCourse}
                 />
+                </ProtectedRoute>
               } />
-              <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+              <Route path="Courses/:cid/*" element={<ProtectedRoute><Courses courses={courses} /></ProtectedRoute>} />
               <Route path="Calendar" element={<h1>Calendar</h1>} />
               <Route path="Inbox" element={<h1>Inbox</h1>} />
             </Routes>
--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import assignmentreducer from "./Courses/Assignments/reducer";
+import accountReducer from "./Account/reducer";
 const store = configureStore({
   reducer: {
     modulesReducer,
-    assignmentreducer
+    assignmentreducer,
+    accountReducer,
   },
 });
 export default store;
